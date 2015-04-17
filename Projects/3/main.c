070634214e81117cbdd59e4225e013257c6fbdd7
Henry Tseng <henrybear327@gmail.com>
2015-04-17 23:32:46 +0800

    Finish parsing function and tested with input up to the moment

--- Projects/3/main.c
+++ Projects/3/main.c
@@ -40,7 +40,7 @@ int main()
     char input_str[1000];
 
     while(fgets(input_str, 1000, stdin) != NULL) {
-        printf("Input %d : %s", input_count++, input_str);
+        printf("Input %d : %s", input_count, input_str);
 
         //check and allocate space for storing info
         if(current_struct + 1 > capacity) {
@@ -59,10 +59,12 @@ int main()
         }
 
         if(parsing(&input, current_struct, input_str) == false || strlen(input_str) < 5) { // 5 because "",,\n
-            printf(" --> Illigal input.\n\n");
+            printf(" --> Illigal input(%d out of %d input).\n\n", input_count - current_struct, input_count);
+            input_count++;
         } else {
-            printf(" --> Input accepted.\n\n");
             current_struct++;
+            input_count++;
+            printf(" --> Input accepted(%d out of total %d input).\n\n", current_struct, input_count);
         }
     }
 
@@ -82,39 +84,82 @@ int parsing(DATA **string, int current_struct, char *input_str)
     char *name = strtok(input_str, ","), *age = strtok(NULL, ","), *gender = strtok(NULL, ",");
 
     if((name == NULL) || (age == NULL) || (gender == NULL)) {
-        printf("Missing name, age, or gender in the input.");
+        printf("Missing comma in the input");
         return false;
-    } else if(strlen(name) == 0 || strlen(age) == 0 || strlen(gender) == 0) {
+    } else if(strlen(name) <= 2 || strlen(age) == 0 || strlen(gender) == 0) {
         printf("Missing name, age, or gender in the input.");
         return false;
     }
 
     //check name
-    if(name[0] != '"' || name[strlen(name)] != '"') {
+    if(name[0] != '"' || name[strlen(name) - 1] != '"') {
         printf("The name in the input isn't surrounded by quotation mark");
         return false;
     }
 
-    int name_length = 0, space = 0;
-    while(name_length <= 16 && name_length != (int)strlen(name) - 1) {
-        if(isalpha(input_str[name_length + 1])) {
+    int name_length = 1, space_count = 0;
+    while(name_length != (int)strlen(name) - 1) {
+        if(name_length > 16) {
+            printf("The name is longer than 16 characters in total.");
+            return false;
+        } else if(isalpha(input_str[name_length]) != false) {
             name_length++;
-        } else if(input_str[name_length + 1] == ' ') {
-            space++;
+        } else if(input_str[name_length] == ' ') {
+            space_count++;
             name_length++;
+        } else if(input_str[name_length] == '"') {
+            printf("More than two quotation marks are entered.(position : %d)", name_length);
+            return false;
         } else {
-            printf("Illigal character detected in name.");
+            printf("Illigal character detected in name.(position : %d)", name_length);
             return false;
         }
     }
+    name_length--;
 
-    if(space == name_length) {
+    if(space_count == name_length) {
         printf("Only spaces in name.");
         return false;
     }
 
+    //check age
+    space_count = 0;
+    for(int i = 0; i < (int)strlen(age); i++) {
+        if(age[i] == ' ') {
+            space_count++;
+        } else if(isdigit(age[i]) == false) {
+            printf("The age entered is not a number.");
+            return false;
+        }
+    }
 
-    //Pass all tests
+    if(space_count == (int)strlen(age)) {
+        printf("Only spaces in age.");
+        return false;
+    }
+
+    //check gender
+    char gender_str[100] = {'\0'};
+    int index = 0;
+    for(int i = 0; i < (int)strlen(gender) - 1; i++) {
+        if(isalpha(gender[i]) == false && gender[i] != ' ') {
+            printf("Illigal character detected in gender.(position : %d)", i);
+            return false;
+        }
+        if('A' <= gender[i] && gender[i] <= 'Z')
+            gender_str[index++] = gender[i] - 'A' + 'a';
+        else if('a' <= gender[i] && gender[i] <= 'z')
+            gender_str[index++] = gender[i];
+    }
+
+    char *valid_gender[] = {"male", "female"};
+    if((strcmp(gender_str, valid_gender[0]) != 0) && (strcmp(gender_str, valid_gender[1]) != 0)) {
+        printf("Wrong gender info is provided. Only male/female is accepted.(Mixed case is accepted)");
+        return false;
+    }
+
+
+//Pass all tests
     printf("The input is checked.");
     return true;
 }

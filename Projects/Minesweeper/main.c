2af61f3e1aa21d6b36bec05e72098a00b39ffab0
Henry Tseng <henrybear327@gmail.com>
2015-05-06 00:32:07 +0800

    Use DP to memorize bombs around

--- Projects/Minesweeper/main.c
+++ Projects/Minesweeper/main.c
@@ -9,9 +9,12 @@ clang-format-3.5 -i -style=LLVM main.c && astyle --style=linux main.c && clang
 -Wall -Wextra main.c -o main
 */
 
-#define BOMB 1
 #define EMPTY 0
-#define CHOSEN 2
+#define BOMB -1
+
+#define CHOSEN -2
+#define FLAG_ON_BOMB -3
+#define FLAG_ON_EMPTY -4
 
 #define DEBUG 1
 
@@ -40,7 +43,7 @@ void print_row_number()
     printf("\n");
 }
 
-void generate_map(int map[][column])
+void generate_map(int map[][column], int bomb_around_count_map[][column])
 {
     while (bomb_count--) {
         int x = rand() % column;
@@ -80,6 +83,18 @@ void generate_map(int map[][column])
     }
 #endif
 
+    // calculate map with bomb around
+    print_row_number();
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < column; j++) {
+            if (map[i][j] == EMPTY)
+                bomb_around_count_map[i][j] = bomb_around_count(map, i, j);
+            else
+                bomb_around_count_map[i][j] = BOMB;
+        }
+        printf("\n");
+    }
+
 #if DEBUG
     // show map with bomb around
     print_row_number();
@@ -87,10 +102,7 @@ void generate_map(int map[][column])
         for (int j = 0; j < column; j++) {
             if (j == 0)
                 printf("%2d ", i + 1);
-            if (map[i][j] == EMPTY)
-                printf("%d ", bomb_around_count(map, i, j)); // can use lazy DP
-            else
-                printf("⊕ ");
+            printf("%d ", bomb_around_count_map[i][j]); // can use lazy DP
         }
         printf("\n");
     }
@@ -151,23 +163,34 @@ int is_bomb(int map[][column], int x, int y)
     }
 }
 
+void place_flag(int map[][column], int x, int y) {}
+
 int main()
 {
     srand(time(NULL));
 
-    printf("Please enter row , column, and bombs:\n");
+    printf("Please enter row , column, and bombs to plant:\n");
     scanf("%d %d %d", &row, &column, &bomb_count);
 
-    int map[row][column];
+    int map[row][column], bomb_around_count_map[row][column];
     memset(map, EMPTY, sizeof(map));
-    generate_map(map);
+    generate_map(map, bomb_around_count_map);
 
     int x, y;
     do {
-        printf("Please enter the location x, y to flip over:\n");
-        scanf("%d %d", &x, &y);
-        x--;
-        y--;
+        int choice;
+        printf("Do you want to 1. place/remove flag 2.flip ?\n");
+        scanf("%d", &choice);
+        if (choice == 1) {
+            place_flag(map, x, y);
+        }
+
+        if (choice == 2) {
+            printf("Please enter the location x, y to flip over:\n");
+            scanf("%d %d", &x, &y);
+            x--;
+            y--;
+        }
     } while (is_bomb(map, x, y) == false);
 
     return 0;

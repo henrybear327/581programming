644e20b87d8be3a6c90aefb135edebc91ed41e8d
Henry Tseng <henrybear327@gmail.com>
2015-05-11 22:13:29 +0800

    Finish reviewing some round 2 questions

--- Weekly Homework/Round 2/Review/main.c
+++ Weekly Homework/Round 2/Review/main.c
@@ -2,12 +2,34 @@
 #include <stdlib.h>
 #include <string.h>
 
+int cmp(const void *a, const void *b)
+{
+    return *(int *)a - *(int *)b;
+}
+
+//clang-format-3.5 -i -style=LLVM main.c && astyle --style=linux main.c && clang -Wall -Wextra main.c -o main && ./main
+
 int main()
 {
     int test_cases;
-    while ("%d", &test_cases) {
-        while (test_cases--) {
+    scanf("%d", &test_cases);
+    while (test_cases--) {
+        int input_count;
+        scanf("%d", &input_count);
+
+        int input[input_count];
+        for (int i = 0; i < input_count; i++)
+            scanf("%d", &input[i]);
+
+        qsort(input, input_count, sizeof(int), cmp);
+
+        int total = 0, index = 0;
+        for (int i = input_count * 2 - 1; i > 0; i -= 2) {
+            // printf("%d %d\n", i, input[i]);
+            total += (i * input[index++]);
         }
+
+        printf("%d\n", total);
     }
 
     return 0;

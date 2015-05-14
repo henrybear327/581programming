cd13b390a991d190b8fe68e3ec8860c7f932c157
Henry Tseng <henrybear327@gmail.com>
2015-05-14 11:56:32 +0800

    Add partial check_queen function

--- Projects/6/main.c
+++ Projects/6/main.c
@@ -6,9 +6,64 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define SIZE 8
+
+#define ALLOWED 0
+#define FORBIDDEN 1
+#define QUEEN 2
+
+typedef struct {
+    int board[SIZE][SIZE];
+    int row;
+} data;
+
+void check_queen(data input)
+{
+    if(input.row == SIZE) {
+        //print solution and count
+    }
+
+    for(int i = 0; i < SIZE; i++) {
+        if(input.board[input.row][i] == ALLOWED) {
+            input.board[input.row][i] = QUEEN; //queen coor. (input.row, i)
+
+            //process board
+            //row
+            for(int index = 0; index < SIZE; index++) {
+                if(input.board[input.row][index] != QUEEN) {
+                    input.board[input.row][index] = FORBIDDEN;
+                }
+            }
+            //column
+            for(int index = 0; index < SIZE; index++) {
+                if(input.board[index][i] != QUEEN) {
+                    input.board[index][i] = FORBIDDEN;
+                }
+            }
+            //diagnal
+            int direction[4][2] = {{-1, -1}, {1, -1}, {-1, 1}, {1, 1}};
+
+
+        }
+    }
+}
+
+int count = 0;
+
 int main()
 {
-	
+    data initial;
+    initial.row = 0;
+    memset(initial.board, ALLOWED, sizeof(initial.board));
+    /*
+    for(int i = 0; i < SIZE; i++) {
+        for(int j = 0; j < SIZE; j++) {
+            printf("%d", initial.board[i][j]);
+        }
+        printf("\n");
+    }
+    */
+    check_queen(initial);
 
-	return 0;
+    return 0;
 }

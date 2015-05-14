40f752729d1a711b4c3ff828888e5a2d7d111ea8
Henry Tseng <henrybear327@gmail.com>
2015-05-14 11:30:34 +0800

    first commit

--- Projects/6/main.c
+++ Projects/6/main.c
@@ -1,3 +1,14 @@
 /*
-輸入N*N格子每個格子0~9表示cost，計算放8 queen的maximum and minimum costs
+輸入N*N格子每個格子0~9表示cost，計算放8 queen的 maximum and minimum costs
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+	
+
+	return 0;
+}

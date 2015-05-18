9ac64a8b5b135ccad10503a57276e4658c5b3cff
Henry Tseng <henrybear327@gmail.com>
2015-05-18 13:40:05 +0000

    Add new 3.cpp

--- Weekly Homework/Round 3/W1/2.c
+++ Weekly Homework/Round 3/W1/2.c
@@ -3,13 +3,32 @@
 #include <string.h>
 #include <stdbool.h>
 
+typedef struct {
+    int pay;
+    int selected;
+    int unselected;
+} INFO;
+
 int main()
 {
     int elements;
-    while (scanf("%d", &elements) != EOF) {
-        int data[elements];
+    while (scanf("%d", &elements) != EOF && elements) {
+        INFO input[elements];
+        memset(input, 0, sizeof(input));
         for (int i = 0; i < elements; i++)
-            scanf("%d", &data[i]);
+            scanf("%d", &input[i].pay);
+
+        input[0].selected = input[0].pay;
+        input[0].unselected = 0;
+        for (int i = 1; i < elements; i++) {
+            input[i].selected = input[i].pay + input[i - 1].unselected;
+            input[i].unselected = (input[i - 1].selected > input[i - 1].unselected
+                                   ? input[i - 1].selected
+                                   : input[i - 1].unselected);
+        }
+        printf("%d\n", input[elements - 1].selected > input[elements - 1].unselected
+               ? input[elements - 1].selected
+               : input[elements - 1].unselected);
     }
 
     return 0;
---
+++ Weekly Homework/Round 3/W1/3.cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+#include <queue>
+
+typedef struct {
+    int rating;
+    int leader;
+    
+    int selected;
+    int unselected;
+} DATA
+
+int main()
+{
+    int test_cases;
+    while(scanf("%d", &test_cases) != EOF) {
+        getchar();
+        
+        while(test_cases--) {
+            int total_members;
+            scanf("%d", &total_members);
+            
+            
+        }
+    }
+    
+    return 0;
+}

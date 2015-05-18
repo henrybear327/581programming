270e2655d578ef6aeb0821993ee2e870a8180807
Henry Tseng <henrybear327@gmail.com>
2015-05-18 22:59:29 +0800

    Finish all Round3 Week1 HW

--- Weekly Homework/Round 3/W1/3.cpp
+++ Weekly Homework/Round 3/W1/3.cpp
@@ -29,46 +29,40 @@ int main()
                 scanf("%d %d", &input[i].leader, &input[i].rating);
                 input[i].selected = input[i].rating;
                 input[input[i].leader].leafCount++;
-
-                printf("i %d %d,  %d %d %d %d %d\n", i, total_members, input[i].rating,
-                       input[i].leader, input[i].leafCount, input[i].selected,
-                       input[i].unselected);
             }
-            printf("here I am\n");
             getchar();
 
-            for (int i = 1; i <= total_members; i++) {
-                printf("i %d,  %d %d %d %d %d", i, input[i].rating, input[i].leader,
-                       input[i].leafCount, input[i].selected, input[i].unselected);
-            }
-
             std::queue<int> trim_leaf;
-            for (int i = 1; i <= total_members; i++) {
-                if (input[i].leafCount == 0) {
-                    input[input[i].leader].selected += input[i].unselected;
-                    input[input[i].leader].unselected +=
-                        MAX(input[i].selected, input[i].unselected);
-                    input[input[i].leader].leafCount--;
 
-                    if (input[input[i].leader].leafCount == 0 && input[i].leader != 1) {
-                        trim_leaf.push(i);
-                    }
-                }
+            /*
+                        for (int i = 1; i <= total_members; i++) {
+                            printf("i %d,  %d %d %d %d %d\n", i, input[i].leader,
+               input[i].rating,
+                                   input[i].leafCount, input[i].selected,
+               input[i].unselected);
+                        }
+            */
+
+            for (int i = 2; i <= total_members; i++) {
+                if (input[i].leafCount == 0)
+                    trim_leaf.push(i);
             }
 
-            while (trim_leaf.size() != 0) {
-                input[input[trim_leaf.front()].leader].selected +=
-                    input[trim_leaf.front()].unselected;
-                input[input[trim_leaf.front()].leader].unselected +=
-                    MAX(input[trim_leaf.front()].selected,
-                        input[trim_leaf.front()].unselected);
-                input[input[trim_leaf.front()].leader].leafCount--;
+            while (trim_leaf.empty() == 0) {
+                int temp = trim_leaf.front();
+                trim_leaf.pop();
 
-                if (input[input[trim_leaf.front()].leader].leafCount == 0 &&
-                    input[trim_leaf.front()].leader != 1) {
-                    trim_leaf.push(trim_leaf.front());
-                }
+                input[input[temp].leader].selected += input[temp].unselected;
+                input[input[temp].leader].unselected +=
+                    MAX(input[temp].selected, input[temp].unselected);
+
+                input[input[temp].leader].leafCount--;
+                if (input[input[temp].leader].leafCount == 0)
+                    trim_leaf.push(input[temp].leader);
+
+                // printf("temp %d\n", temp);
             }
+
             printf("%d\n", MAX(input[1].selected, input[1].unselected));
         }
     }

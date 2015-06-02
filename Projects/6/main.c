d93dc7fe8b63c310f1fd3ad3d79795f48aea9b31
Henry Tseng <henrybear327@gmail.com>
2015-06-03 00:11:20 +0800

    Modify 8-queen to calculate max and min

--- Projects/6/main.c
+++ Projects/6/main.c
@@ -6,7 +6,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define SIZE 8
+//#define SIZE 8
+int SIZE;
 
 #define ALLOWED 0
 #define FORBIDDEN 1
@@ -15,54 +16,62 @@
 int count = 0;
 
 typedef struct {
-    int board[SIZE][SIZE];
+    int board[15][15];
     int row;
 } data;
 
+int weight[15][15], max, min;
+
 void check_queen(data input_old)
 {
-    if(input_old.row == SIZE) {
-        //print solution and count
+    if (input_old.row == SIZE) {
+        // print solution and count
         count++;
-        printf("%d\n", count);
-        for(int i = 0; i < SIZE; i++) {
-            for(int j = 0; j < SIZE; j++) {
-                printf("%d", input_old.board[i][j]);
+        // printf("%d\n", count);
+        int temp = 0;
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                // printf("%d", input_old.board[i][j]);
+                if (input_old.board[i][j] == QUEEN)
+                    temp += weight[i][j];
             }
-            printf("\n");
+            // printf("\n");
         }
+        // printf("temp %d max %d min %d\n", temp, max, min);
+        max = temp > max ? temp : max;
+        min = temp < min ? temp : min;
 
         return;
     }
 
-    for(int i = 0; i < SIZE; i++) {
-        if(input_old.board[input_old.row][i] == ALLOWED) {
+    for (int i = 0; i < SIZE; i++) {
+        if (input_old.board[input_old.row][i] == ALLOWED) {
             data input = input_old;
-            input.board[input.row][i] = QUEEN; //queen coordinate (input.row, i)
+            input.board[input.row][i] = QUEEN; // queen coordinate (input.row, i)
 
-            //process board
-            //row
-            for(int index = 0; index < SIZE; index++) {
-                if(input.board[input.row][index] != QUEEN) {
+            // process board
+            // row
+            for (int index = 0; index < SIZE; index++) {
+                if (input.board[input.row][index] != QUEEN) {
                     input.board[input.row][index] = FORBIDDEN;
                 }
             }
-            //column
-            for(int index = 0; index < SIZE; index++) {
-                if(input.board[index][i] != QUEEN) {
+            // column
+            for (int index = 0; index < SIZE; index++) {
+                if (input.board[index][i] != QUEEN) {
                     input.board[index][i] = FORBIDDEN;
                 }
             }
-            //diagnal
+            // diagnal
             int row = input.row, column = i;
-            for(int index = 1; index < SIZE; index++) {
-                if(row - index >= 0 && column - index >= 0) //upper-left
+            for (int index = 1; index < SIZE; index++) {
+                if (row - index >= 0 && column - index >= 0) // upper-left
                     input.board[row - index][column - index] = FORBIDDEN;
-                if(row + index < SIZE && column - index >= 0)  //upper-right
+                if (row + index < SIZE && column - index >= 0) // upper-right
                     input.board[row + index][column - index] = FORBIDDEN;
-                if(row - index >= 0 && column + index < SIZE)  //lower-left
+                if (row - index >= 0 && column + index < SIZE) // lower-left
                     input.board[row - index][column + index] = FORBIDDEN;
-                if(row + index < SIZE && column + index < SIZE)  //lower-right
+                if (row + index < SIZE && column + index < SIZE) // lower-right
                     input.board[row + index][column + index] = FORBIDDEN;
             }
             input.row++;
@@ -76,11 +85,25 @@ void check_queen(data input_old)
 
 int main()
 {
-    data initial;
-    initial.row = 0;
-    memset(initial.board, ALLOWED, sizeof(initial.board));
+    int test_cases;
+    while (scanf("%d", &test_cases) != EOF) {
+        while (test_cases--) {
+            scanf("%d", &SIZE);
+            max = 0;
+            min = 2147483647;
+            for (int i = 0; i < SIZE; i++)
+                for (int j = 0; j < SIZE; j++)
+                    scanf("%d", &weight[i][j]);
 
-    check_queen(initial);
+            data initial;
+            initial.row = 0;
+            memset(initial.board, ALLOWED, sizeof(initial.board));
+
+            check_queen(initial);
+
+            printf("%d %d\n", max, min);
+        }
+    }
 
     return 0;
 }
